7c2ecaa115827b1aa68b67b62cb742d68a9d5099
ggkh-autogenix <gopiaimlworks@gmail.com>
2025-05-06 17:01:30 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,15 +1,18 @@
 from twilio.rest import Client
 import os
 
-account_sid = os.getenv("TWILIO_SID")
-auth_token = os.getenv("TWILIO_TOKEN")
-twilio_number = os.getenv("TWILIO_WHATSAPP")
+def send_whatsapp(to, message):
+    # Trim to 100 words
+    words = message.split()
+    trimmed_message = ' '.join(words[:100])  # Limit to first 100 words
 
-client = Client(account_sid, auth_token)
+    account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+    auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+    from_whatsapp_number = os.getenv("TWILIO_FROM_WHATSAPP")
 
-def send_whatsapp(to_number, message):
+    client = Client(account_sid, auth_token)
     client.messages.create(
-        body=message,
-        from_=f'whatsapp:{twilio_number}',
-        to=f'whatsapp:{to_number}'
+        body=trimmed_message,
+        from_=f'whatsapp:{from_whatsapp_number}',
+        to=f'whatsapp:{to}'
     )
