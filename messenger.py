adcc4cdd1b183a699dc593566f594ed63784e3e6
ggkh-autogenix <gopiaimlworks@gmail.com>
2025-05-06 17:56:08 +0530

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -1,8 +1,7 @@
 from twilio.rest import Client
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+
 
 def send_whatsapp(message: str):
     client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
